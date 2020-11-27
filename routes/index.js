4fe79063c52040bf897957be1cbcdb4fab9cde65
prasetyarinaldii <prasetyarinaldi55@gmail.com>
2020-11-27 10:59:29 +0700

    add fitur google oauth server

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const Authentication = require('../middlewares/authentication')
 
 router.post('/register', User.register)
 router.post('/login', User.login)
+router.post('/googleLogin', User.googleLogin)
 router.use(Authentication)
 router.post('/predict', User.predict)
 
